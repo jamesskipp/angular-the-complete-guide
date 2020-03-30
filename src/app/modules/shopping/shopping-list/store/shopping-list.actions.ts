dd13afae3a0401b23a5b0f1c7d0043eb7a9eaebd
jskipp <jamesskipp@gmail.com>
2020-03-30 16:13:15 -0400

    353

--- src/app/modules/shopping/shopping-list/store/shopping-list.actions.ts
+++ src/app/modules/shopping/shopping-list/store/shopping-list.actions.ts
@@ -3,6 +3,8 @@ import { Ingredient } from 'src/app/modules/ingredients/models/ingredient.model'
 
 export const ADD_INGREDIENT = 'ADD_INGREDIENT';
 export const ADD_INGREDIENTS = 'ADD_INGREDIENTS';
+export const UPDATE_INGREDIENTS = 'UPDATE_INGREDIENTS';
+export const DELETE_INGREDIENT = 'DELETE_INGREDIENT';
 
 export class AddIngredient implements Action {
   readonly type = ADD_INGREDIENT;
@@ -14,6 +16,18 @@ export class AddIngredients implements Action {
   readonly type = ADD_INGREDIENTS;
 
   constructor(public payload: Ingredient[]) {}
+}
+
+export class UpdateIngredients implements Action {
+  readonly type = UPDATE_INGREDIENTS;
+
+  constructor(public payload: { index: number; ingredient: Ingredient }) {}
+}
+
+export class DeleteIngredient implements Action {
+  readonly type = DELETE_INGREDIENT;
+
+  constructor(public payload: { index: number }) {}
 }
 
 export type ShoppingListActions = AddIngredient | AddIngredients;
