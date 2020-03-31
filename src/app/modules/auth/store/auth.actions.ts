07c5929d4061abd9b76e04e1e281c84ba15bd7f2
jskipp <jamesskipp@gmail.com>
2020-03-31 10:45:29 -0400

    365

--- src/app/modules/auth/store/auth.actions.ts
+++ src/app/modules/auth/store/auth.actions.ts
@@ -1,5 +1,6 @@
 import { Action } from '@ngrx/store';
 
+export const LOGIN_START = '[Auth] Login Start';
 export const LOGIN = '[Auth] Login';
 export const LOGOUT = '[Auth] Logout';
 
@@ -20,4 +21,8 @@ export class Logout implements Action {
   readonly type = LOGOUT;
 }
 
-export type AuthActions = Login | Logout;
+export class LoginStart implements Action {
+  readonly type = LOGIN_START;
+}
+
+export type AuthActions = Login | Logout | LoginStart;
