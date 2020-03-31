cd585f8ef21d79ac2211958507e3b301e94f9d33
jskipp <jamesskipp@gmail.com>
2020-03-31 11:30:32 -0400

    366

--- src/app/modules/auth/store/auth.effects.ts
+++ src/app/modules/auth/store/auth.effects.ts
@@ -1,9 +1,43 @@
 import { Actions, ofType } from '@ngrx/effects';
 
 import * as AuthActions from './auth.actions';
+import { switchMap, catchError, map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { AuthResponseData } from '../models/auth-response-data.model';
+import { HttpClient } from '@angular/common/http';
+import { of } from 'rxjs';
 
 export class AuthEffects {
-  authLogin = this.actions$.pipe(ofType(AuthActions.LOGIN_START));
+  constructor(private actions$: Actions, private http: HttpClient) {}
 
-  constructor(private actions$: Actions) {}
+  authLogin = this.actions$.pipe(
+    ofType(AuthActions.LOGIN_START),
+    switchMap((authData: AuthActions.LoginStart) => {
+      return this.http
+        .post<AuthResponseData>(environment.auth.loginUrl, {
+          email: authData.payload.email,
+          password: authData.payload.password,
+          returnSecureToken: true,
+        })
+        .pipe(
+          catchError((error) => {
+            of();
+          }),
+          map((response) => {
+            of();
+          })
+        );
+      // .pipe(
+      //   catchError(this.handleError),
+      //   tap((response) => {
+      //     this.handleAuth(
+      //       response.email,
+      //       response.localId,
+      //       response.idToken,
+      //       parseInt(response.expiresIn)
+      //     );
+      //   })
+      // );
+    })
+  );
 }
